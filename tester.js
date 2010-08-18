0bb776f2f28fbaa8f185be48db84862b8fdc4b9b
Brian Mavity <brian@brianmavity.com>
2010-08-18 16:37:47 -0500

    getting junk backed up before organize

---
+++ tester.js
@@ -0,0 +1,7 @@
+var sys = require('sys'),
+    fs = require('fs'),
+    ometa = require('../ometa-js/ometa-node');
+
+fs.readFile('./scss.ometa', function(err, contents) {
+  fs.writeFile('results.css', ometa(contents.toString()).toString().replace(/\\n/g, '\n')); 
+});
