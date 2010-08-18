8fa837445fb1eb563388563fe750c246f7ea7503
Brian Mavity <brian@brianmavity.com>
2010-08-18 16:58:46 -0500

    moved code files to src directory

--- src/tester.js
+++ src/tester.js
@@ -1,6 +1,6 @@
 var sys = require('sys'),
     fs = require('fs'),
-    ometa = require('../ometa-js/ometa-node');
+    ometa = require('../../ometa-js/ometa-node');
 
 fs.readFile('./scss.ometa', function(err, contents) {
   fs.writeFile('results.css', ometa(contents.toString()).toString().replace(/\\n/g, '\n')); 
