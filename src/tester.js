a85e168aa52624a44d819bf07cd1807163444f18
Brian Mavity <brian@brianmavity.com>
2010-08-18 20:17:30 -0500

    modified to have grammar accept external input

--- src/tester.js
+++ src/tester.js
@@ -2,6 +2,9 @@ var sys = require('sys'),
     fs = require('fs'),
     ometa = require('../lib/ometa-js/ometa-node');
 
-fs.readFile('./scss.ometa', function(err, contents) {
-  fs.writeFile('results.css', ometa(contents.toString()).toString().replace(/\\n/g, '\n')); 
+fs.readFile('scss.ometa', function(err, contents) {
+  var parser = ometa(contents.toString());
+  fs.readFile('input.scss', function(err, cssFile) {
+    fs.writeFile('results.css', parser(cssFile.toString()).toString().replace(/\\n/g, '\n')); 
+  });
 });
