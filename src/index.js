3c74e8c0c4d4587892f6324b93d14f6eee1c362b
Brian Mavity <brian@brianmavity.com>
2010-08-19 15:37:07 -0500

    Added external api for scss

---
+++ src/index.js
@@ -0,0 +1,40 @@
+var sys = require('sys'),
+    fs = require('fs'),
+    ometa = require('../lib/ometa'),
+    createdParser;
+
+var getParser = function(callback) {
+  if(!createdParser) {
+    fs.readFile('scss.ometa', function(err, contents) {
+      if(err) {
+        callback(err);
+      } else {
+        ometa.createParser(contents.toString(), function(err, parser) {
+          if(err) {
+            callback(err);
+          } else {
+            createdParser = parser;
+            callback(null, createdParser);
+          }
+        });
+      }
+    });
+  } else {
+    callback(null, createdParser);
+  }
+};
+
+var parse = function(scssFile, callback) {
+  getParser(function(err, parser) {
+    if(err) {
+      callback(err);
+    } else {
+      parser.parse(scssFile, function(err, css) {
+        callback(err, css.toString().replace(/\\n/g, '\n'));
+      });
+    }
+  });
+});
+
+
+module.exports.parse = parseFile;
