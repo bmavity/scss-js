d48c36cc0a2e7f0405d171938472ec0a40ce8935
Brian Mavity <brian@brianmavity.com>
2010-08-19 15:18:31 -0500

    Modified external ometa api to follow node conventions with callbacks

--- src/tester.js
+++ src/tester.js
@@ -1,10 +1,13 @@
 var sys = require('sys'),
     fs = require('fs'),
-    ometa = require('../lib/ometa-js/ometa-node');
+    ometa = require('../lib/ometa');
 
 fs.readFile('scss.ometa', function(err, contents) {
-  var parser = ometa(contents.toString());
-  fs.readFile('input.scss', function(err, cssFile) {
-    fs.writeFile('results.css', parser(cssFile.toString()).toString().replace(/\\n/g, '\n')); 
+  ometa.createParser(contents.toString(), function(err, parser) {
+    fs.readFile('input.scss', function(err, scssFile) {
+      parser.parse(scssFile.toString(), function(err, css) {
+        fs.writeFile('results.css', css.toString().replace(/\\n/g, '\n')); 
+      });
+    });
   });
 });
