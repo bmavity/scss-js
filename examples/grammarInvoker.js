73cfa000736042ffe9c5dcfff2c8ad23d81c79f2
Brian Mavity <brian@brianmavity.com>
2010-09-27 10:28:58 -0500

    Implemented CssSelector grammar in Scss grammar

--- examples/grammarInvoker.js
+++ examples/grammarInvoker.js
@@ -38,6 +38,6 @@ var parse = function(scssFile, callback) {
 
 
 module.exports.parse = parse;
-parse('a:nth-child(2 3)', function(err) {
+parse(':not(h1, h2, h3)', function(err) {
   sys.puts(sys.inspect(err, true, null));
 });
